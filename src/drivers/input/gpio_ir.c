0a412fc3cd4f5f6063968637c5202d92f6bd3c46
Sean Cross <sean@chumby.com>
2011-07-03 03:44:04 +0800

    gpio-ir: Refactor IR driver to actually work
    The existing IR driver from the vendor had serious issues, including
    just plain not working.  This refactors the driver to be more Linuxey.
    
    It is still hardcoded to work with the Nikon remotes we have in-office.
    
    Eventually it would be nice to add Lirc support, but that's a project
    for another day.

--- src/drivers/input/gpio_ir.c
+++ src/drivers/input/gpio_ir.c
@@ -12,6 +12,9 @@
  * published by the Free Software Foundation.
  *
  */
+
+#define DEBUG
+
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/delay.h>
@@ -31,9 +34,6 @@
 #include <asm/gpio.h>
 
 MODULE_LICENSE("GPL");
-
-/* Only and only one XXX_READOUT_IRCBUFFER can be defined */
-#define MARVELL_READOUT_IRCBUFFER
 
 /*
  * Nec SC33 code:
@@ -73,176 +73,95 @@ static unsigned int cb_start=0, cb_end=0;
 /*
  * Table of IR-signal-code and key
  */
-ir_key_table_t marvell_nec_ir_key_table[] = {
-
-	{0x00, MV_IR_KEY_DIGIT_1},
-	{0x01, MV_IR_KEY_DIGIT_2},
-	{0x02, MV_IR_KEY_DIGIT_3},
-	{0x03, MV_IR_KEY_DIGIT_4},
-	{0x04, MV_IR_KEY_DIGIT_5},
-	{0x05, MV_IR_KEY_DIGIT_6},
-	{0x06, MV_IR_KEY_DIGIT_7},
-	{0x07, MV_IR_KEY_DIGIT_8},
-	{0x08, MV_IR_KEY_DIGIT_9},
-	{0x09, MV_IR_KEY_DIGIT_10},
-	{0x0A, MV_IR_KEY_DIGIT_11},
-	{0x0B, MV_IR_KEY_DIGIT_12},
-	{0x0C, MV_IR_KEY_DIGIT_13},
-	{0x0D, MV_IR_KEY_DIGIT_14},
-	{0x0E, MV_IR_KEY_DIGIT_15},
-	{0x0F, MV_IR_KEY_DIGIT_16},
-	{0x10, MV_IR_KEY_DIGIT_17},
-	{0x11, MV_IR_KEY_DIGIT_18},
-	{0x12, MV_IR_KEY_DIGIT_19},
-	{0x13, MV_IR_KEY_DIGIT_20},
-	{0x14, MV_IR_KEY_DIGIT_21},
-	{0x15, MV_IR_KEY_DIGIT_22},
-	{0x16, MV_IR_KEY_DIGIT_23},
-	{0x17, MV_IR_KEY_DIGIT_24},
-	{0x18, MV_IR_KEY_DIGIT_25},
-	{0x19, MV_IR_KEY_DIGIT_26},
-	{0x1A, MV_IR_KEY_DIGIT_27},
-	{0x1B, MV_IR_KEY_DIGIT_28},
-	{0x1C, MV_IR_KEY_DIGIT_29},
-	{0x1D, MV_IR_KEY_DIGIT_30},
-	{0x1E, MV_IR_KEY_DIGIT_31},
-	{0x1F, MV_IR_KEY_DIGIT_32},
-	{0x20, MV_IR_KEY_DIGIT_33},
-	{0, MV_IR_KEY_NULL},
-
+ir_key_table_t nikon_key_table[] = {
+	{0xb946f685, KEY_CONFIG},
+	{0xba45f685, KEY_VENDOR},
+	{0xbc43f685, KEY_UP},
+	{0xbd42f685, KEY_LEFT},
+	{0xbf40f685, KEY_ENTER},
+	{0xbe41f685, KEY_RIGHT},
+	{0xbb44f685, KEY_DOWN},
+	{0xb748f685, KEY_1},
+	{0xb847f685, KEY_2},
+	{0,          0},
 };
 
-ir_key_table_t *ir_key_table = marvell_nec_ir_key_table;
-
-unsigned int ir_key = MV_IR_KEY_NULL;
-unsigned int ir_key_recvflag = 0;
-
-/*
- * structure to store the time when interrupt comes
- */
-typedef struct system_time {
-	unsigned int jiff;
-	unsigned int base;
-} system_time_t;
-system_time_t len_time = {0, 0};
-
-/*
- * analyse the level length based on NEC protocol.
- * NEC ir transmitter timing:
- * Preamble: 9ms/4.5ms/0.56ms, Bit1: 1.69ms/0.56ms, Bit0: 0.56ms/0.56ms.
- * Hold: 9ms/2.25ms/0.56ms
- */
-static unsigned int word = 0, dont_send_more=0 , count_word = 0 , preamble = 0;
-static unsigned int end = 0, i=0;
+ir_key_table_t *ir_key_table = nikon_key_table;
 
 
 
-/*
- *Analyse the address and key
- *
- */
-static void analyseword (struct cir_device *cir, int word)
+static int decode_command(struct cir_device *cir, unsigned long cmd)
 {
-	if ((word << CODE_SIZE) == (CUSTOM_CODE << CODE_SIZE) && !dont_send_more) {
-		if ((word & 0xff000000)==(~(word << 8) & 0xff000000)) {
-			ir_key = MV_IR_KEY_NULL;
-			i = 0;
-	/* check for the key */
-			while (ir_key_table[i].ir_key != MV_IR_KEY_NULL) {
-				if ((ir_key_table[i].ir_encode) == ((word & 0xff0000)
-					>> CODE_SIZE)) {
-					ir_key = ir_key_table[i].ir_key;
-	/*				printk(KERN_INFO "The key %d was found\n",ir_key); */
-					input_report_key(cir->input_dev, ir_key , 1);
-					input_report_key(cir->input_dev, ir_key , 0);
-					cb_start = cb_end;
-					dont_send_more = 1;
-					break;
-				}
-
-				i++;
-			}
-			if (ir_key == MV_IR_KEY_NULL) {
-				printk(">>> Unable to find matching key for %x\n", word);
-			}
-			else {
-				printk(">>> Found matching key! %d\n", ir_key);
-			}
-			/* key not found, just noises */
+	ir_key_table_t *key = ir_key_table;
+	while (key->ir_encode) {
+		if (key->ir_encode == cmd) {
+			dev_dbg(&cir->pdev->dev,
+				"Found key %d\n", key->ir_key);
+			input_report_key(cir->input_dev, key->ir_key , 1);
+			input_report_key(cir->input_dev, key->ir_key , 0);
+			return 0;
 		}
+		key++;
 	}
-	else {
-		uart_putc('N');
-	}
-}
-static void clear_ircbuffer (struct cir_device *cir)
-{
-	unsigned int event_len;
-
-	while ((cb_start != cb_end) && (((cb_start+1) & CIRC_BUFF_MASK) != cb_end)) {
-		event_len = cbuffer[cb_start]*1000;
-		/*printk(KERN_INFO "%u\n",event_len); */
-		cb_start++;
-		cb_start &= CIRC_BUFF_MASK;
-		if ((event_len > ZERO_MIN) && (event_len < ZERO_MAX)) {
-			uart_putc('0');
-			if (preamble == 1) {
-				count_word++;
-			}
-			if (count_word == word_size) {
-				analyseword (cir, word);
-				count_word = 0;
-				preamble = 0;
-				word = 0;
-			}
-			continue;
-		}
-		if ((event_len >= ONE_MIN) && (event_len < ONE_MAX)) {
-			uart_putc('1');
-			if (preamble == 1) {
-				word |= 1 << count_word;
-				count_word++;
-			}
-			if (count_word == word_size) {
-				analyseword (cir, word);
-				count_word = 0;
-				preamble = 0;
-				word = 0;
-			}
-			continue;
-		}
-		if ((event_len > PREAM_MIN) && (event_len < PREAM_MAX)) {
-			uart_putc('P');
-			preamble = 1;
-			count_word = 0;
-			end = 0;
-			continue;
-		}
-		if ((event_len > END_MIN) && (event_len < END_MAX)){
-			uart_putc('S');
-			continue;
-		}
-		if (event_len > ENDED){
-			uart_putc('E');
-			dont_send_more = 0;
-			continue;
-		}
-	}
+	dev_err(&cir->pdev->dev, "No key found for code 0x%08x\n", cmd);
+	return 1;
 }
 
 /*
  * This function should be called in an independent thread.
  * It reads out bit-wised code.
  */
-static void process_times(unsigned long nodata)
+static void process_times(unsigned long data)
 {
+	struct cir_device *cir = (struct cir_device *)data;
 	int event_count = 0;
+	unsigned long cmd;
+	unsigned int cmd_shift;
+	unsigned long length, active;
+
+	cmd = 0;
+	cmd_shift = 0;
+
 	while (cb_start != cb_end) {
 		event_count++;
-		printk("Event %ld ticks long: %d\n", (cbuffer[cb_start] & ~0x80000000L), cbuffer[cb_start] >> 31);
+		length = cbuffer[cb_start] & ~0x80000000L;
+		active = !(cbuffer[cb_start] >> 31);
+
+		/* Advance to the next spot in the buffer */
 		cb_start = (cb_start+1) & CIRC_BUFF_MASK;
+
+		/* For this protocol, we only consider the rising edges */
+		if (!active)
+			continue;
+
+		/* Convert from ticks to usecs */
+		length = (length*10000)>>15;
+
+
+		/* Start-of-packet messages are about 4.5 ms */
+		if (length > 2000 || length == 0) {
+			cmd_shift = 0;
+			continue;
+		}
+
+		else if (length <= 2000 && length > 1300)
+			cmd |= (1<<cmd_shift++);
+		else if (length <= 700 && length > 400)
+			cmd |= (0<<cmd_shift++);
+		else
+			dev_err(&cir->pdev->dev,
+				"Event %lu doesn't match anything\n", length);
+
+		//dev_dbg(&cir->pdev->dev, "Event %lu usec long: %lu\n",
+		//	length, active);
+
 	}
-	printk("Read out %d events\n", event_count);
+	if (cmd)
+		decode_command(cir, cmd);
+
+	if (cmd)
+		dev_dbg(&cir->pdev->dev, "Read out %d events: 0x%08x\n",
+			event_count, cmd);
 	return;
 }
 
@@ -256,6 +175,13 @@ static irqreturn_t cir_interrupt(int irq, void *dev_id)
 
 	prev = this;
 	this = timer_services_counter_read(COUNTER_0);
+
+	/* This is true when the buffer has just been emptied, so
+	 * the time will need to be 0.
+	 */
+	if (cb_end == cb_start)
+		prev = this;
+
 	cbuffer[cb_end] = ((this - prev) & 0x7fffffffL)
 		 	| ((__raw_readl(cir->mmio_base + 0x00) & 0x40) << 25);
 
@@ -263,6 +189,7 @@ static irqreturn_t cir_interrupt(int irq, void *dev_id)
 	if (cb_end == cb_start)
 		cb_start = (cb_start+1) & CIRC_BUFF_MASK;
 
+	process_time_timer.data = (unsigned long)cir;
 	mod_timer(&process_time_timer, jiffies + msecs_to_jiffies(20));
 
 	return IRQ_HANDLED;
@@ -323,6 +250,7 @@ static int __devinit cir_probe(struct platform_device *pdev)
 	struct cir_device *cir;
 	struct resource *res;
 	int ret = 0;
+	int key;
 
    	cir = kzalloc(sizeof(struct cir_device), GFP_KERNEL);
    	if (cir == NULL) {
@@ -377,21 +305,17 @@ static int __devinit cir_probe(struct platform_device *pdev)
 	cir->input_dev->phys = "aspenite_cir/input2";
 	cir->input_dev->dev.parent = &pdev->dev;
 	cir->input_dev->evbit[0] = BIT(EV_KEY);
-	cir->pdev = pdev->dev.platform_data;
+	cir->pdev = pdev;
 
 	/* Announce that the CIR will generate  key map */
-	ir_key = MV_IR_KEY_NULL;
-	for (i=0; i< 33;) {
-		ir_key = ir_key_table[i].ir_key;
-		set_bit(ir_key, cir->input_dev->keybit);
-		i++;
-	}
-	dev_dbg(&pdev->dev, " key map was set \n");
+	for (key=0; ir_key_table[key].ir_key; key++)
+		set_bit(ir_key_table[key].ir_key, cir->input_dev->keybit);
+	dev_dbg(&pdev->dev, "Key map was set \n");
 
 	/* Register with the input subsystem */
 	if (input_register_device(cir->input_dev)) 
-		dev_dbg(&pdev->dev, "can't register CIR_input Driver.\n");
-	dev_dbg(&pdev->dev, "CIR_input Driver Initialized.\n");
+		dev_err(&pdev->dev, "Can't register CIR_input Driver.\n");
+	dev_dbg(&pdev->dev, "Driver Initialized.\n");
 
 	return 0;
 	
@@ -423,40 +347,15 @@ static int __devexit cir_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static int __devinit aspenite_cir_probe(struct platform_device *pdev)
-{
-	return cir_probe(pdev);
-}
-
 static struct platform_driver aspenite_cir_driver = {
 	.driver		= {
 		.name	= "aspenite-cir",
 	},
-	.probe		= aspenite_cir_probe,
+	.probe		= cir_probe,
 	.remove		= __devexit_p(cir_remove),
 	.suspend	= cir_suspend,
 	.resume		= cir_resume,
 };
-
-/**
- * cir_init - setup the cir port
- *
- * initialise  cir.
- *
- */
-
-int __init cir_init(struct cir_dev *dev)
-{
-	if (gpio_request(dev->cir->pin, "IR_PIN")) {
-		printk(KERN_ERR "Request GPIO failed,"
-				"gpio: %d \n", dev->cir->pin);
-		return -EIO;
-	}
-
-	/* Direction is input */
-	gpio_direction_input(dev->cir->pin);
-	return 0;
-}
 
 
 static int __init aspenite_cir_init(void)
